eab807e9e7d3034947e50b916fedaac28dbb0bc8
ezegyfa2 <darvasvok@gmail.com>
2024-11-17 09:07:12 +0200

    Build

---
+++ public/build/assets/PrivacyPolicy-3PhYLAjM.js
@@ -0,0 +1,1 @@
+import{M as o}from"./MainLayout-DnBe8n5X.js";import{d as r,o as t,c as a}from"./app-CxgqLvlt.js";import"./Notifications.vue_vue_type_style_index_0_lang-CE56YSkp.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=r({__name:"PrivacyPolicy",setup(e){return(c,n)=>(t(),a(o))}});export{_ as default};
---
+++ public/build/assets/TermsAndConditions-C9i-N5Tw.js
@@ -0,0 +1,1 @@
+import{M as o}from"./MainLayout-DnBe8n5X.js";import{d as t,o as e,c as r}from"./app-CxgqLvlt.js";import"./Notifications.vue_vue_type_style_index_0_lang-CE56YSkp.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=t({__name:"TermsAndConditions",setup(n){return(a,s)=>(e(),r(o))}});export{_ as default};
