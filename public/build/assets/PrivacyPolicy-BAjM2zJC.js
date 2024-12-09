06487bd0262ba2dd71b8ec8492178e7edb928c07
ezegyfa2 <darvasvok@gmail.com>
2024-12-09 13:37:35 +0200

    Build

---
+++ public/build/assets/PrivacyPolicy-BAjM2zJC.js
@@ -0,0 +1,1 @@
+import{M as o}from"./MainLayout-B9XqzM1S.js";import{d as r,o as t,c as a}from"./app-Dmi3YrTx.js";import"./Notifications.vue_vue_type_style_index_0_lang-D1HdMzS9.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=r({__name:"PrivacyPolicy",setup(e){return(c,n)=>(t(),a(o))}});export{_ as default};
