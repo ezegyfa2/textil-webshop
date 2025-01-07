6b18573ad13a233c0c5b4ad21e8ce5f4aaf8596e
ezegyfa2 <darvasvok@gmail.com>
2025-01-07 17:47:20 +0200

    Fixing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -181,10 +181,11 @@ abstract class Controller
         if ($imageData) {
             $imageData['relative_path'] = str_replace('/storage/uploads/', '', $imageData['url']);
             if (!array_key_exists('id', $imageData)) {
-                //$model->image->delete();
+                $oldImage = $model->image;
                 $image = $model->image()->create(['relative_path' => $imageData['relative_path']]);
                 $model->image()->associate($image);
                 $model->save();
+                $oldImage->delete();
                 $image->moveFromUploads();
                 $image->createResizedVersions();
             }
