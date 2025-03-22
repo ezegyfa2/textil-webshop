c9878dc3cb025d328ed65ad0497e6e31bd445db0
ezegyfa2 <darvasvok@gmail.com>
2025-03-22 15:12:50 +0100

    Build

---
+++ public/build/assets/TermsAndConditions-Cxv6aDJp.js
@@ -0,0 +1,1 @@
+import{M as o}from"./MainLayout-DqCqPa_3.js";import{d as t,o as e,c as r}from"./app-CbWqvH8J.js";import"./Notifications.vue_vue_type_style_index_0_lang-CHhSBP3G.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=t({__name:"TermsAndConditions",setup(n){return(a,s)=>(e(),r(o))}});export{_ as default};
