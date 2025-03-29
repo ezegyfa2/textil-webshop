e9e06c7a0fcfcf4120907eaa218c085777809f7f
ezegyfa2 <darvasvok@gmail.com>
2025-03-29 11:32:41 +0100

    Build

---
+++ public/build/assets/ReturnPolicy-ZR8n5n5i.js
@@ -0,0 +1,1 @@
+import{M as o}from"./MainLayout-Ce-3ah1z.js";import{d as t,o as e,c as r}from"./app-Cb4xbh_X.js";import"./Notifications.vue_vue_type_style_index_0_lang-CqkleMIL.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const _=t({__name:"ReturnPolicy",setup(a){return(n,c)=>(e(),r(o))}});export{_ as default};
